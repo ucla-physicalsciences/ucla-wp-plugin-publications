f7dad2a69402db2703fb04679377143ccb99c692
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-05-19 09:51:12 -0700

    Update ucla-publications.php
    addition of the book entry

--- ucla-publications.php
+++ ucla-publications.php
@@ -26,3 +26,9 @@ add_action('admin_init', 'admin_init_article');
 
 require_once (PUB_PATH . '/php/ucla-custom-fields-publications.php');
 add_action('save_post', 'save_publication_details_article');
+
+require_once (PUB_PATH . '/php/ucla-custom-fields-publications.php');
+add_action('admin_init', 'admin_init_book');
+
+require_once (PUB_PATH . '/php/ucla-custom-fields-publications.php');
+add_action('save_post', 'save_publication_details_book');
