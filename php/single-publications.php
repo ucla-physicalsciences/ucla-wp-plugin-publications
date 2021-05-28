413bf5587cd4ada5205118ddb2058079a05dd8f6
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-05-28 16:02:19 -0700

    Update single-publications.php
    adding comment

--- php/single-publications.php
+++ php/single-publications.php
@@ -35,41 +35,40 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
     <?php endwhile; endif; ?>
 <div class="col span_3_of_12">
 <ul>
-<?php if(get_post_meta($post->ID, 'publication_author', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_author', true)):?> //makes the article's author detail optional
 <li>
- Author: <?php echo get_post_meta($post->ID, 'publication_author', true);?>
+ Author: <?php echo get_post_meta($post->ID, 'publication_author', true);?> //set the author detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_journal', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_journal', true)):?>  //makes the article's journal detail optional
 <li>
- Journal: <?php echo get_post_meta($post->ID, 'publication_journal', true);?>
+ Journal: <?php echo get_post_meta($post->ID, 'publication_journal', true);?> //set the journal detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_volume', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_volume', true)):?> //makes the article's volume detail optional
 <li>
- Volume: <?php echo get_post_meta($post->ID, 'publication_volume', true);?>
+ Volume: <?php echo get_post_meta($post->ID, 'publication_volume', true);?> //set the volume detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_pages', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_pages', true)):?> //makes the article's pages detail optional
 <li>
- Pages: <?php echo get_post_meta($post->ID, 'publication_pages', true);?>
+ Pages: <?php echo get_post_meta($post->ID, 'publication_pages', true);?> //set the pages detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_ISSN', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_ISSN', true)):?> //makes the article's ISSN detail optional
 <li>
- ISSN: <?php echo get_post_meta($post->ID, 'publication_ISSN', true);?>
+ ISSN: <?php echo get_post_meta($post->ID, 'publication_ISSN', true);?> //set the ISSN detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if( get_post_meta($post->ID, 'publication_DOI', true)):?>
-
+<?php if( get_post_meta($post->ID, 'publication_DOI', true)):?> //makes the article's DOI detail optional
 <li>
- DOI: <?php echo get_post_meta($post->ID, 'publication_DOI', true);?>
+ DOI: <?php echo get_post_meta($post->ID, 'publication_DOI', true);?> //set the DOI detail typed in by the user on the page
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_URL', true)):?>
+<?php if(get_post_meta($post->ID, 'publication_URL', true)):?> //makes the article's URL detail optional
 <?php
-            echo '<li> URL: <a href="' . get_post_meta($post->ID, 'publication_URL', true). '"> link to publication</a></li>';?>
-<?php endif;?>
+            echo '<li> URL: <a href="' . get_post_meta($post->ID, 'publication_URL', true). '"> link to publication</a></li>';?> //set the URL detail typed in by the user on the page as a clickable link
+<?php endif;?> 
 </ul>
     </div>
 
