bc095590ac29f1e5df6e267a9ccd26b9e5f2aa80
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-05-30 14:28:57 -0700

    Update single-publications.php
    comment

--- php/single-publications.php
+++ php/single-publications.php
@@ -33,41 +33,42 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
       </div>
 
     <?php endwhile; endif; ?>
+           <!-- All of the above is the regular  page within UCLA child theme, below, we set the columnn at the right of the content to be a listing of all the item listed inn article, to note, here they are all optional so if the user doesn't file one or multiples, no error would come up and the line associated will just not be present.-->
 <div class="col span_3_of_12">
 <ul>
-<?php if(get_post_meta($post->ID, 'publication_author', true)):?> //makes the article's author detail optional
+<?php if(get_post_meta($post->ID, 'publication_author', true)):?> <!-- makes the article's author detail optional-->
 <li>
- Author: <?php echo get_post_meta($post->ID, 'publication_author', true);?> //set the author detail typed in by the user on the page
+ Author: <?php echo get_post_meta($post->ID, 'publication_author', true);?> <!--set the author detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_journal', true)):?>  //makes the article's journal detail optional
+<?php if(get_post_meta($post->ID, 'publication_journal', true)):?>  <!--makes the article's journal detail optional-->
 <li>
- Journal: <?php echo get_post_meta($post->ID, 'publication_journal', true);?> //set the journal detail typed in by the user on the page
+ Journal: <?php echo get_post_meta($post->ID, 'publication_journal', true);?> <!--set the journal detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_volume', true)):?> //makes the article's volume detail optional
+<?php if(get_post_meta($post->ID, 'publication_volume', true)):?> <!--makes the article's volume detail optional-->
 <li>
- Volume: <?php echo get_post_meta($post->ID, 'publication_volume', true);?> //set the volume detail typed in by the user on the page
+ Volume: <?php echo get_post_meta($post->ID, 'publication_volume', true);?> <!--set the volume detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_pages', true)):?> //makes the article's pages detail optional
+<?php if(get_post_meta($post->ID, 'publication_pages', true)):?> <!--makes the article's pages detail optional-->
 <li>
- Pages: <?php echo get_post_meta($post->ID, 'publication_pages', true);?> //set the pages detail typed in by the user on the page
+ Pages: <?php echo get_post_meta($post->ID, 'publication_pages', true);?> <!--set the pages detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_ISSN', true)):?> //makes the article's ISSN detail optional
+<?php if(get_post_meta($post->ID, 'publication_ISSN', true)):?> <!--makes the article's ISSN detail optional-->
 <li>
- ISSN: <?php echo get_post_meta($post->ID, 'publication_ISSN', true);?> //set the ISSN detail typed in by the user on the page
+ ISSN: <?php echo get_post_meta($post->ID, 'publication_ISSN', true);?> <!--set the ISSN detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if( get_post_meta($post->ID, 'publication_DOI', true)):?> //makes the article's DOI detail optional
+<?php if( get_post_meta($post->ID, 'publication_DOI', true)):?> <!--makes the article's DOI detail optional-->
 <li>
- DOI: <?php echo get_post_meta($post->ID, 'publication_DOI', true);?> //set the DOI detail typed in by the user on the page
+ DOI: <?php echo get_post_meta($post->ID, 'publication_DOI', true);?><!--set the DOI detail typed in by the user on the page-->
 </li>
 <?php endif;?>
-<?php if(get_post_meta($post->ID, 'publication_URL', true)):?> //makes the article's URL detail optional
+<?php if(get_post_meta($post->ID, 'publication_URL', true)):?> <!--makes the article's URL detail optional-->
 <?php
-            echo '<li> URL: <a href="' . get_post_meta($post->ID, 'publication_URL', true). '"> link to publication</a></li>';?> //set the URL detail typed in by the user on the page as a clickable link
+            echo '<li> URL: <a href="' . get_post_meta($post->ID, 'publication_URL', true). '"> link to publication</a></li>';?> <!--set the URL detail typed in by the user on the page as a clickable link-->
 <?php endif;?> 
 </ul>
     </div>
