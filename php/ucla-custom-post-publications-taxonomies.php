30ff168b0fa482ee88a6889e9575a68488c5fd9f
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-05-30 14:23:34 -0700

    Update ucla-custom-post-publications-taxonomies.php
    add comment

--- php/ucla-custom-post-publications-taxonomies.php
+++ php/ucla-custom-post-publications-taxonomies.php
@@ -1,22 +1,23 @@
 <?php
+//year category
 function register_date_taxonomy()
 {
-        $labels = array(
+        $labels = array( //set label of our taxonomy
                 'name' => __('Years', PUB_DOMAIN),
                 'singular_name' => __('Year', PUB_DOMAIN),
                 'add_new_item' => __('Add New Year', PUB_DOMAIN),
         );
 
         $args = array(
-                'labels' => $labels,
-                'public' => true,
+                'labels' => $labels, //retrieve values set in $labels above
+                'public' => true, 
                 'show_admin_column' => true,
                 'show_in_quick_edit' => true,
                 'show_in_rest' => 'false',
-                'hierarchical' => true, /* category */
+                'hierarchical' => true, /*. this is a category taxonomy */
         );
 
-        $post_types = array ( 'publication');
+        $post_types = array ( 'publication'); //this taxonomy is only present for publications
 
         register_taxonomy('year', $post_types, $args);
 
