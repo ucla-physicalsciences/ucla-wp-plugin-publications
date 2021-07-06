19622b577aaaafb7df6b79be36f6927ff45b04f8
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-07-05 20:40:13 -0700

    Update single-article.php

--- single-article.php
+++ single-article.php
@@ -13,7 +13,33 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
       <div class="ucla campus">
         <div class="col span_12_of_12">
           <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-          <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+          <p><b><?php $author_id = intval( get_query_var( 'author' ) ); echo get_the_author_meta( 'user_login', $author_id ); ?></b>, <?php the_title(); ?>,  <em><?php if(get_post_meta($post->ID, '_article_journal', true)):
+          echo get_post_meta($post->ID, '_article_journal', true);
+          endif;?>
+          </em>,
+<?php if(get_post_meta($post->ID, '_article_volume', true)):
+          echo get_post_meta($post->ID, '_article_volume', true);
+          endif;?>, (
+<?php if(get_post_meta($post->ID, '_article_number', true)):
+          echo get_post_meta($post->ID, '_article_number', true);
+          endif;?>), (p.
+<?php if(get_post_meta($post->ID, '_article_pages', true)):
+          echo get_post_meta($post->ID, '_article_pages', true);
+          endif;?>), doi:
+<?php if(get_post_meta($post->ID, '_article_DOI', true)):
+          echo get_post_meta($post->ID, '_article_DOI', true);
+          endif;?>,  (
+<?php if(get_post_meta($post->ID, '_article_month', true)):
+          echo get_post_meta($post->ID, '_article_month', true);
+          endif;?>/
+<?php if(get_post_meta($post->ID, '_article_year', true)):
+          echo get_post_meta($post->ID, '_article_year', true);
+          endif;?>).
+
+
+
+
+ </p>
         </div>
       </div>
     </header>
@@ -21,8 +47,6 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
     <div class="ucla campus">
 
       <div class="col span_9_of_12">
-        <time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time>
-        <p><?php $author_id = intval( get_query_var( 'author' ) ); echo get_the_author_meta( 'user_login', $author_id ); ?></p>
         <?php the_content(); ?>
 
         <?php
@@ -37,9 +61,6 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
 
     <?php endwhile; endif; ?>
 
-    <div class="col span_3_of_12">
-      <?php get_sidebar(); ?>
-    </div>
 
   </div>
 
