7e9f7d5ec8a491d6f26ac2e6dad511513f214094
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-07-09 03:30:59 -0700

    Create gutenberg-publication-block.js

---
+++ gutenberg-publication-block.js
@@ -0,0 +1,19 @@
+const {registerBlockType} = wp.blocks;
+
+registerBlockType('ucla-publications/my-publication',{
+
+        //built-in attributes
+
+        title: 'Academic Publications',
+        description: 'Block that displays Academic Publications',
+        icon: 'dashicons-analytics',
+        category: 'embeds',
+
+        //custom attributes
+
+        attributes: {},
+        //custom functions
+        //built-in functions
+        edit() {},
+        save() {}
+});
